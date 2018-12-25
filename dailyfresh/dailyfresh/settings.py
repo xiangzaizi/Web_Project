cf848cd96420d2f4d41fe8645518f7682a32b5df
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-12-25 19:42:21 +0800

    全文检索   2.1 安装和配置: 全文检索框架(haystack)+搜索引擎(whoosh)。   2.2 生成索引文件。   2.3 全文检索使用。   2.4 更改分词方式: jieba分词
     仅完成setting的文件配置

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -157,3 +157,14 @@ FDFS_CLIENT_CONF = os.path.join(BASE_DIR, 'utils/fdfs/client.conf')
 
 # 指定FDFS系统中Nginx的ip和port
 FDFS_NGINX_URL = 'http://172.16.179.131:8888/'
+
+# 全文检索框架配置
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        # 使用whoosh引擎
+        # 'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
+        # 索引文件路径
+        'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
+    }
+}
