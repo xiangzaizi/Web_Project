d83034100154eb90df0d49a8b7534a795183ce71
xiangzaizi <skyxiang.jiang@gmail.com>
2019-01-05 15:02:44 +0800

    summery之celery网站首页静态页面的生成

--- dailyfresh/knowledge/celery_email_static.py
+++ dailyfresh/knowledge/celery_email_static.py
@@ -1,6 +1,8 @@
 from celery import Celery
 from django.conf import settings
 from django.core.mail import send_mail
+from apps.goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+
 
 # 初始化django运行所依赖的环境变量
 # 这两行代码在启动worker一端打开
@@ -35,5 +37,54 @@ def send_register_active_email(to_email, username, token):
     # 异步启动celery监听主程序发邮件的请求
 
 # 项目启动: celery -A 任务函数所在文件的路径 worker -l info
+
+
+@app.task
+def generate_static_index_html():
+    """生成静态首页文件"""
+    # 获取商品的分类信息
+    types = GoodsType.objects.all()
+
+    # 获取首页的轮播商品的信息
+    index_banner = IndexGoodsBanner.objects.all().order_by('index')
+
+    # 获取首页的促销活动的信息
+    promotion_banner = IndexPromotionBanner.objects.all().order_by('index')
+
+    # 获取首页分类商品的展示信息
+    for type in types:
+        # 获取type种类在首页展示的图片商品的信息和文字商品的信息
+        # QuerySet
+        image_banner = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1)
+        title_banner = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0)
+
+        # 给type对象增加属性title_banner, image_banner
+        # 分别保存type种类在首页展示的文字商品和图片商品的信息
+        type.title_banner = title_banner
+        type.image_banner = image_banner
+
+    # 判断用户是否已登录
+    cart_count = 0
+
+    # 组织模板上下文
+    context = {
+        'types': types,
+        'index_banner': index_banner,
+        'promotion_banner': promotion_banner,
+        'cart_count': cart_count
+    }
+
+    # 使用模板
+    # 1. 加载模板文件
+    from django.template import loader
+    temp = loader.get_template('static_index.html')
+
+    # 2. 模板渲染
+    static_html = temp.render(context)
+
+    # 生成静态首页文件
+    save_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+    with open(save_path, 'w') as f:
+        f.write(static_html)
 
 
